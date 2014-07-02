12054c8f7fdc26f0169fbce46fce38a7837ece19
saosey <saosey@yahoo.com>
2014-07-02 02:29:12 +0100

    new agents-list header view with notification badge added

---
+++ app/scripts/views/agents-list-header-view.js
@@ -0,0 +1,39 @@
+/**
+ * @name AgentsListHeaderView
+ * @description list header with dynamic badge displaying number of agents
+ * @param {function} require
+ */
+define(function (require) {
+    'use strict';
+
+    var _ = require('underscore'),
+        Backbone = require('backbone'),
+        Agent = require('models/agent'),
+        AgentListView = require('collections/agents'),
+        AgentView = require('views/agent-view'),
+        AgentsListHeaderTemplate = require('text!../templates/agents-list-header-view.html'),
+        EventManager = require('vent'),
+        AgentsListHeaderView;
+
+    AgentsListHeaderView = Backbone.View.extend({
+
+        tagName: 'header',
+        className: 'list-header',
+        template: _.template(AgentsListHeaderTemplate),
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.collection, 'all', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template());
+
+            this.$el.find('.num-of-agents').text(this.collection.length);
+            return this;
+        }
+    });
+
+    return AgentsListHeaderView;
+});
