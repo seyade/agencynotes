1af0a173e19095a71a801fe761f5bc276005dea4
seyade <oluade.git@outlook.com>
2014-07-05 14:44:03 +0100

    Added docs and comments

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,9 +1,15 @@
+/**
+ * @name Application
+ * @description main entry point to application. Settings can be added to Application object.
+ * @param {function} require
+ */
 define(function (require) {
 
     var _ = require('underscore'),
         Backbone = require('backbone'),
         AppRoutes = require('routers/app-routes');
-
+    
+    // entry point object.
     var Application = {
         start: function() {
             var app = new AppRoutes;
